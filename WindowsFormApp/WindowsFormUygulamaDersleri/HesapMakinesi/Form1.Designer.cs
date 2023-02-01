703f4e79e0eefa7954337b0793cc0ea5c3aa12a8
Atakan <atakanbektas.c@gmail.com>
2023-02-01 17:26:27 +0300

    e devlet

---
+++ WindowsFormApp/WindowsFormUygulamaDersleri/HesapMakinesi/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace HesapMakinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.lblScreen = new System.Windows.Forms.Label();
+            this.btnTopla = new System.Windows.Forms.Button();
+            this.btnCikart = new System.Windows.Forms.Button();
+            this.btnCarp = new System.Windows.Forms.Button();
+            this.btnBol = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSayi1.Location = new System.Drawing.Point(39, 59);
+            this.txtSayi1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(213, 26);
+            this.txtSayi1.TabIndex = 0;
+            this.txtSayi1.Click += new System.EventHandler(this.txtSayi1_Click_1);
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSayi2.Location = new System.Drawing.Point(39, 99);
+            this.txtSayi2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(213, 26);
+            this.txtSayi2.TabIndex = 0;
+            this.txtSayi2.Click += new System.EventHandler(this.txtSayi2_Click_1);
+            // 
+            // lblScreen
+            // 
+            this.lblScreen.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblScreen.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblScreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblScreen.ForeColor = System.Drawing.Color.Lime;
+            this.lblScreen.Location = new System.Drawing.Point(35, 130);
+            this.lblScreen.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblScreen.Name = "lblScreen";
+            this.lblScreen.Size = new System.Drawing.Size(217, 119);
+            this.lblScreen.TabIndex = 1;
+            this.lblScreen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnTopla
+            // 
+            this.btnTopla.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTopla.Location = new System.Drawing.Point(273, 59);
+            this.btnTopla.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnTopla.Name = "btnTopla";
+            this.btnTopla.Size = new System.Drawing.Size(63, 41);
+            this.btnTopla.TabIndex = 2;
+            this.btnTopla.Text = "+";
+            this.btnTopla.UseVisualStyleBackColor = true;
+            this.btnTopla.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCikart
+            // 
+            this.btnCikart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCikart.Location = new System.Drawing.Point(273, 110);
+            this.btnCikart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCikart.Name = "btnCikart";
+            this.btnCikart.Size = new System.Drawing.Size(63, 41);
+            this.btnCikart.TabIndex = 2;
+            this.btnCikart.Text = "-";
+            this.btnCikart.UseVisualStyleBackColor = true;
+            this.btnCikart.Click += new System.EventHandler(this.btnCikart_Click);
+            // 
+            // btnCarp
+            // 
+            this.btnCarp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCarp.Location = new System.Drawing.Point(273, 161);
+            this.btnCarp.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCarp.Name = "btnCarp";
+            this.btnCarp.Size = new System.Drawing.Size(63, 41);
+            this.btnCarp.TabIndex = 2;
+            this.btnCarp.Text = "*";
+            this.btnCarp.UseVisualStyleBackColor = true;
+            this.btnCarp.Click += new System.EventHandler(this.btnCarp_Click);
+            // 
+            // btnBol
+            // 
+            this.btnBol.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBol.Location = new System.Drawing.Point(273, 208);
+            this.btnBol.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnBol.Name = "btnBol";
+            this.btnBol.Size = new System.Drawing.Size(63, 41);
+            this.btnBol.TabIndex = 2;
+            this.btnBol.Text = "/";
+            this.btnBol.UseVisualStyleBackColor = true;
+            this.btnBol.Click += new System.EventHandler(this.btnBol_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(69, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "HESAP MAKİNESİ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::HesapMakinesi.Properties.Resources.channels4_profile;
+            this.ClientSize = new System.Drawing.Size(374, 295);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBol);
+            this.Controls.Add(this.btnCarp);
+            this.Controls.Add(this.btnCikart);
+            this.Controls.Add(this.btnTopla);
+            this.Controls.Add(this.lblScreen);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Hesap Makinesi Uygulaması";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.Label lblScreen;
+        private System.Windows.Forms.Button btnTopla;
+        private System.Windows.Forms.Button btnCikart;
+        private System.Windows.Forms.Button btnCarp;
+        private System.Windows.Forms.Button btnBol;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormApp/WindowsFormUygulamaDersleri/HesapMakinesi/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblScreen.Text = (Convert.ToInt32(txtSayi1.Text) + Convert.ToInt32(txtSayi2.Text)).ToString();
+        }
+
+        private void btnCikart_Click(object sender, EventArgs e)
+        {
+            lblScreen.Text = (Convert.ToInt32(txtSayi1.Text) - Convert.ToInt32(txtSayi2.Text)).ToString();
+        }
+
+        private void btnCarp_Click(object sender, EventArgs e)
+        {
+            lblScreen.Text = (Convert.ToInt32(txtSayi1.Text) * Convert.ToInt32(txtSayi2.Text)).ToString();
+        }
+
+        private void btnBol_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(txtSayi2.Text)==0)
+            {
+                MessageBox.Show("0'a bölünemez!");
+                txtSayi2.Text = "";
+            }
+            else
+            {
+            lblScreen.Text = (Convert.ToInt32(txtSayi1.Text) / Convert.ToInt32(txtSayi2.Text)).ToString();
+            }
+        }
+
+     
+
+        private void txtSayi1_Click_1(object sender, EventArgs e)
+        {
+            txtSayi1.Text = "";
+            lblScreen.Text = "";
+        }
+
+        private void txtSayi2_Click_1(object sender, EventArgs e)
+        {
+            txtSayi2.Text = "";
+            lblScreen.Text = "";
+        }
+    }
+}
