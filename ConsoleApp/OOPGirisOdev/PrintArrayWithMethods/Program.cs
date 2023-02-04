70edeb3f67c45bba3eddd70259c9c324c0812d4c
Atakan <atakanbektas.c@gmail.com>
2023-02-04 20:08:32 +0300

    2.Soru Tamamlandı.

---
+++ ConsoleApp/OOPGirisOdev/PrintArrayWithMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintArrayWithMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[9] { 17, 222, 30, -4, -15, 60, 0, 12, 21 };
+            PrintArray(nums);
+            Console.ReadKey();
+        }
+        static void PrintArray(int[] nums)
+        {
+            int i = 0;
+            while (i < 9)
+            {
+                Console.WriteLine(string.Format("{1}.Sayınız = {0}", nums[i++],i));
+            }
+
+        }
+
+    }
+}
---
+++ ConsoleApp/OOPGirisOdev/RecursiveMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Faktoriyeli Hesaplanacak sayıyı giriniz.!");
+            Console.WriteLine($"Faktoriyelinizin sonucu = {GetFact(int.Parse(Console.ReadLine()))}");
+            Console.Read();
+        }
+
+        private static int GetFact(int num)
+        {
+            if (num==1)
+            {
+                return 1;
+            }
+            return num * GetFact(num - 1);
+        }
+    }
+}
