c75d317581ef0eeeaf849bd88c3fbb60e877c387
Atakan <atakanbektas.c@gmail.com>
2023-02-04 20:30:42 +0300

    3. Soru Tamamlandı
    Params ve Recursive Method kullanarak Sums Metodu kullanıldı.

---
+++ ConsoleApp/OOPGirisOdev/SumsMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumsMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine(GetSum(sum,int.Parse(Console.ReadLine())));
+        }
+
+        private static int GetSum(int sum, params int[] nums)
+        {
+            sum += nums[0];
+            Console.Write($"Toplam = {sum} + ");
+            return GetSum(sum, int.Parse(Console.ReadLine()));
+        }
+    }
+}
