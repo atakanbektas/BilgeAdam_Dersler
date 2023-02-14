bf264a3653558736d8ac17e45ddf26fe58bb270c
Atakan <atakanbektas.c@gmail.com>
2023-02-14 14:01:39 +0300

    update

---
+++ WindowsFormApp/WindowsFormUygulamaDersleri/RichTextBoxSinirlama/SifreZorlukDerecesi/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace SifreZorlukDerecesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblLevel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(115, 25);
+            this.txtSifre.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(188, 26);
+            this.txtSifre.TabIndex = 0;
+            this.txtSifre.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ŞİFRE";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(99, 67);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Şifre Zorluk Derecesi";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.ForeColor = System.Drawing.Color.Red;
+            this.lblLevel.Location = new System.Drawing.Point(99, 97);
+            this.lblLevel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(192, 20);
+            this.lblLevel.TabIndex = 1;
+            this.lblLevel.Text = "Çok Düşük";
+            this.lblLevel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 141);
+            this.Controls.Add(this.lblLevel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSifre);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblLevel;
+    }
+}
+
---
+++ WindowsFormApp/WindowsFormUygulamaDersleri/RichTextBoxSinirlama/SifreZorlukDerecesi/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SifreZorlukDerecesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string sayilar = "0123456789";
+        string ozelKarakterler = "!'^+%&/()=?_-";
+        string buyukHarfler = "QWERTYUIOPĞÜASDFGHJKLŞİZXCVBNMÖÇ";
+        int sayacSayi = 0;
+        int sayacBuyukHarf = 0;
+        int sayacOzelKarakter = 0;
+        int sayacUzunluk = 0;
+        int sayac = 0;
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {           
+
+            if (txtSifre.Text.Length < 6)
+            {
+                sayacBuyukHarf = 0;
+                sayacOzelKarakter = 0;
+                sayacSayi = 0;
+                sayacUzunluk = 0;
+            }
+            else if (txtSifre.Text.Length >= 6)
+            {
+                sayacUzunluk = 1;
+                if (sayilar.Contains(txtSifre.Text[txtSifre.Text.Length-1]))
+                {
+                    sayacSayi = 1;
+                }                
+                else if (ozelKarakterler.Contains(txtSifre.Text[txtSifre.Text.Length-1]))
+                {
+                    sayacOzelKarakter = 1;
+                }
+                else if (buyukHarfler.Contains(txtSifre.Text[txtSifre.Text.Length-1]))
+                {
+                    sayacBuyukHarf = 1;
+                }
+               
+            }
+
+            sayac = sayacBuyukHarf + sayacOzelKarakter + sayacSayi + sayacUzunluk;
+            if (sayac == 0)
+            {
+                lblLevel.Text = "Çok Düşük";
+                lblLevel.ForeColor = Color.Red;
+            }
+            else if (sayac == 1)
+            {
+                lblLevel.Text = "Düşük";
+                lblLevel.ForeColor = Color.OrangeRed;
+            }
+            else if (sayac == 2)
+            {
+                lblLevel.Text = "Orta";
+                lblLevel.ForeColor = Color.DarkOrange;
+            }
+            else if (sayac == 3)
+            {
+                lblLevel.Text = "Yüksek";
+                lblLevel.ForeColor = Color.Green;
+            }
+            else if (sayac == 4)
+            {
+                lblLevel.Text = "Çok Yüksek";
+                lblLevel.ForeColor = Color.DarkGreen;
+            }
+        }
+    }
+}
