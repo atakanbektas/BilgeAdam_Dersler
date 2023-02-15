87e92b3d258f7c6abc3da1847cdae72a1371a37a
Atakan <atakanbektas.c@gmail.com>
2023-02-15 16:58:35 +0300

    Class properties field örnekleri yapıldı.

---
+++ ConsoleApp/BenMutevaziDegilim/BenMutevaziDegilim/Atakan.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenMutevaziDegilim
+{
+    public class Atakan
+    {
+
+        public static List<string> Hobileri = new List<string>(); 
+        public string AdSoyad { get; set; }
+        public DateTime DogumTarihi { get; set; }
+        public string BildigiProglamlamaDilleri { get; set; }
+        // public string Hobi { get; set; }
+        private string myVar;
+
+        public string Hobi
+        {
+            get { return myVar; }
+            set 
+            {
+                Hobileri.Add(value);
+                myVar = value; 
+            }
+        }
+
+        public string EhliyetDurumu { get; set; }
+        public string CalabildigiEnstrumanlar { get; set; }
+        public string Meslegi { get; set; }
+        public int Yas { get; set; }
+
+        public Atakan()
+        {
+            Console.WriteLine("Ben bugün OOP'ye başladım.\n");
+        }
+
+
+
+        public void Topla()
+        {
+
+        }
+
+    }
+}
---
+++ ConsoleApp/BenMutevaziDegilim/BenMutevaziDegilim/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenMutevaziDegilim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Atakan atakan = new Atakan();
+
+
+
+            atakan.AdSoyad = "Atakan Bektaş";
+            atakan.BildigiProglamlamaDilleri = "C#";
+            atakan.DogumTarihi = Convert.ToDateTime("22.02.1995");
+            atakan.CalabildigiEnstrumanlar = "Gitar,Yan Flüt";
+            atakan.Hobi = "Tiyatro";
+            atakan.Hobi = "Müzik";
+            atakan.Hobi = "Yüzme";
+            atakan.Hobi = "Aykut beyle sohbet";
+
+            atakan.Meslegi = "Çok yıldız yazılımcı";
+            atakan.Yas = 27;
+
+
+            Console.WriteLine("Bilgiler yükleniyor.");
+            System.Threading.Thread.Sleep(2000);
+
+            Console.WriteLine($"Adı Soyadı = {atakan.AdSoyad}\n");
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine($"Dogum Tarihi = {atakan.DogumTarihi.ToString("dd MMMM yyyy")}\n");
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine($"Yaşı = {atakan.Yas}\n");
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine($"Meslegi = {atakan.Meslegi}\n");
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine($"Bildigi Proglamlama Dilleri = {atakan.BildigiProglamlamaDilleri}\n");
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine($"Calabildigi Enstrumanlar = {atakan.CalabildigiEnstrumanlar}\n");
+
+            foreach (var item in Atakan.Hobileri)
+            {
+                Console.WriteLine($"Hobileri = {item}");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
