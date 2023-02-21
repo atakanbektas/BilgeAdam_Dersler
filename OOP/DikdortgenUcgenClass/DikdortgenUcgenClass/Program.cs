dfdb04d0be84eaa5b8adc17ae8d34bc3fb989363
Atakan <atakanbektas.c@gmail.com>
2023-02-21 16:04:08 +0300

    Uygulama Dersi güncelleme.

---
+++ OOP/DikdortgenUcgenClass/DikdortgenUcgenClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DikdortgenUcgenClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(new Ucgen().Ciz() + "\n\n\n\n");
+            Console.WriteLine(new Dikdörtgen().Ciz());
+
+            Console.WriteLine(2>>1);
+           
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP/DikdortgenUcgenClass/DikdortgenUcgenClass/Ucgen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DikdortgenUcgenClass
+{
+    internal class Ucgen : IIslemYapilabilir
+    {
+        public Ucgen()
+        {
+            Console.WriteLine("Ucgen Olusturuldu.");
+
+        }
+
+        public string Ciz()
+        {
+            return "Ucgen Çizildi";
+        }
+    }
+}
