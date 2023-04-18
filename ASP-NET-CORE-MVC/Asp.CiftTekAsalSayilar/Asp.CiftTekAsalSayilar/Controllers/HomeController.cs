ece6067535bfb0bb442afc1b7bb323b90cdf4002
Atakan <atakanbektas.c@gmail.com>
2023-04-18 17:48:49 +0300

    asp.net ödev

---
+++ ASP-NET-CORE-MVC/Asp.CiftTekAsalSayilar/Asp.CiftTekAsalSayilar/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.CiftTekAsalSayilar.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Sayilar()
+        {
+            List<int> sayilar = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            return View(sayilar);
+        }
+        S
+
+    }
+}
