d900b6c94ce02da0cae5bd160669a53bf4ac6c08
Atakan <atakanbektas.c@gmail.com>
2023-04-19 12:58:58 +0300

    Yeni mvc projesi oluşturuldu, ürünler table'da listelendi.

---
+++ ASP-NET-CORE-MVC/Uygulama1904/Uygulama1904/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Uygulama1904.Models;
+
+namespace Uygulama1904.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View(Product.Urunler);
+        }
+
+    }
+}
---
+++ ASP-NET-CORE-MVC/Uygulama1904/Uygulama1904/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Uygulama1904.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
