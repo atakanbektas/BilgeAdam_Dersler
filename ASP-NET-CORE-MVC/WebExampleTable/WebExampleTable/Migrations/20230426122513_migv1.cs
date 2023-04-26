ebde041097d0a753c6de801a42dbc7bd6632bc55
Atakan <atakanbektas.c@gmail.com>
2023-04-26 17:48:08 +0300

    .

---
+++ ASP-NET-CORE-MVC/WebExampleTable/WebExampleTable/Migrations/20230426122513_migv1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebExampleTable.Migrations
+{
+    public partial class migv1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Kisiler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AdSoyad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DogumTarihi = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kisiler", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Kisiler");
+        }
+    }
+}
