89a299f01240e47b2e2fe2faf287719319764027
Atakan <atakanbektas.c@gmail.com>
2023-05-03 01:18:45 +0300

    eski proje kaldırıldı.

--- ASP-NET-CORE-MVC/Category-Product-Crud/Category-Product-Crud/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Category_Product_Crud.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
