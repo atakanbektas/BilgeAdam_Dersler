57bd6f76719fd8c6d59f45ede67bfe214c07aa31
Atakan <atakanbektas.c@gmail.com>
2023-05-02 17:16:51 +0300

    .

---
+++ ASP-NET-CORE-MVC/Category-Product-Crud/Category-Product-Crud/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Category_Product_Crud.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Category_Product_Crud.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly MyContext _context;
+
+        public ProductController(MyContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var product = _context.Products.ToList();
+            return View();
+        }
+    }
+}
