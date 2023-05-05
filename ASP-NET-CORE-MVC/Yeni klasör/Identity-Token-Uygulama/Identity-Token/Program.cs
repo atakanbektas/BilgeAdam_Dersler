1970892f7748a753f8aa6bae28523f4b4b20b67e
Atakan <atakanbektas.c@gmail.com>
2023-05-05 23:10:59 +0300

    .

--- ASP-NET-CORE-MVC/Yeni klasör/Identity-Token-Uygulama/Identity-Token/Program.cs
+++ ASP-NET-CORE-MVC/Yeni klasör/Identity-Token-Uygulama/Identity-Token/Program.cs
@@ -1,7 +1,12 @@
+using Identity_Token.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<NorthwindContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("NortwindConnectionString")));
+
 
 var app = builder.Build();
 
